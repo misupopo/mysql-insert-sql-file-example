6d1852f65664ea1c1d550f5091ec399a5e704be1
misupopo <kokorononakaniitumo@yahoo.co.jp>
2022-12-23 11:10:31 +0900

    modify index priority

--- child.sql
+++ child.sql
@@ -4,8 +4,8 @@ CREATE TABLE `child`
     `Attribute` varchar(200) NOT NULL,
     `Parent`    int(10) NOT NULL,
     PRIMARY KEY (`Name`),
---     CONSTRAINT `Attribute_fk` FOREIGN KEY (`Attribute`) REFERENCES `parent` (`Attribute`)
-    CONSTRAINT `Parent_fk` FOREIGN KEY (`Parent`) REFERENCES `parent` (`Parent`)
+    CONSTRAINT `Attribute_fk` FOREIGN KEY (`Attribute`) REFERENCES `parent` (`Attribute`)
+--     CONSTRAINT `Parent_fk` FOREIGN KEY (`Parent`) REFERENCES `parent` (`Parent`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- parent.sql
+++ parent.sql
@@ -1,13 +1,13 @@
-CREATE TABLE `parent`
-(
-    `Parent`    int(10) NOT NULL,
-    `Attribute` varchar(200) NOT NULL,
-    PRIMARY KEY (
-                 `Parent`,
-                 `Attribute`
-        )
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+-- CREATE TABLE `parent`
+-- (
+--     `Parent`    int(10) NOT NULL,
+--     `Attribute` varchar(200) NOT NULL,
+--     PRIMARY KEY (
+--                  `Parent`,
+--                  `Attribute`
+--         )
+-- ) ENGINE = InnoDB
+--   DEFAULT CHARSET = utf8mb4;
 
 -- CREATE TABLE `parent`
 -- (
@@ -19,3 +19,13 @@ CREATE TABLE `parent`
 --         )
 -- ) ENGINE = InnoDB
 --   DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE `parent`
+(
+    `Parent`    int(10) NOT NULL,
+    `Attribute` varchar(200) NOT NULL,
+    PRIMARY KEY (`Parent`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+ALTER TABLE parent ADD INDEX index02(Attribute)
