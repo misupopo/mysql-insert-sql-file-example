084433c35f1f2246ca4a981acafd892121b873df
misupopo <kokorononakaniitumo@yahoo.co.jp>
2022-12-23 11:28:36 +0900

    add case01

---
+++ case01/child.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `batch_master_record_batch`
+(
+    `Product`               varchar(40) NOT NULL,
+    `BusinessPartner`       int(12) NOT NULL,
+    `Plant`                 varchar(4) NOT NULL,
+    `Batch`                 varchar(10) NOT NULL,
+    `ValidityStartDate`     date NOT NULL,
+    `ValidityEndDate`       date NOT NULL,
+    `CountryOfOrigin`       varchar(3) DEFAULT NULL,
+    `ManufactureDate`       date DEFAULT NULL,
+    `CreationDateTime`      datetime DEFAULT NULL,
+    `LastChangeDateTime`    datetime DEFAULT NULL,
+    `IsMarkedForDeletion`   tinyint(1) DEFAULT NULL,
+
+    PRIMARY KEY (`BusinessPartner`, `Product`, `Plant`, `Batch`),
+
+    CONSTRAINT `BatchMasterRecordBatchDataProduct_fk` FOREIGN KEY (`Product`, `BusinessPartner`) REFERENCES `product_master_business_partner_data` (`Product`, `BusinessPartner`),
+
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
